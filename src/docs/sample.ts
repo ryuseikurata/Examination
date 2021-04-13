6d90dd9292e43b9bd1a25ff9ea117e65f3f8c4f8
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-04-13 22:25:06 +0900

    feat: add sample

---
+++ src/docs/sample.ts
@@ -0,0 +1,46 @@
+
+class Main {
+  reader: any;
+
+  constructor() {
+    /// 標準入力
+
+    this.reader = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+  }
+
+  execute() {
+    const lines: string[] = []
+    this.reader.on('line', (line: string) => {
+      lines.push(line);
+    });
+    this.reader.on('close', () => {
+      const input = (lines[0] as string).split(" ");
+      if (input[0] && input[1]) {
+        const a = +Number(input[0]);
+        const b = +Number(input[1]);
+        const res = a - b * 2;
+
+        if (res > 0) {
+          console.log(res);
+        } else {
+          console.log(0);
+        }
+      }
+      console.log('the end!')
+    });
+  }
+
+}
+
+new Main().execute();
+
+/*
+npm ts-node src/docs/input.ts
+> 5 1
+command + C
+the end!
+*/
