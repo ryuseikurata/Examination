fc23f7dc4f6d2dba25bbc4f24049432f61eb12e8
シュレック倉田 <32031454+ryuseikurata@users.noreply.github.com>
2021-04-14 22:33:31 +0900

    feat: add maximumProfit

--- src/rasen_book/part2/ALDS1_1_D/maximum-profit.ts
+++ src/rasen_book/part2/ALDS1_1_D/maximum-profit.ts
@@ -1,3 +1,4 @@
+import { Prompt } from './../../../docs/sample';
 /**
  * @description FX取引では、異なる国の通貨を交換することで為替さの利益を得ることができます。例えば、1どる100円の時に1000ドル買い、価格変動により1どる108円になった時にうると、(108円 - 100円) * 1000ドル = 8000円の利益を得ることができます。
  * ある通貨について、時刻tにおける価格Rt(t=0,1,2...n-1)が入力として与えられるので、価格の差Rj-Ri(ただし、j > i)の最大値を求めてください。
@@ -6,19 +7,15 @@
  * @limit 2<=n<=200,000
  * 1<=R_t<=10^9
  */
-export class MaximumProfit {
-  /**
-   *
-   * 1. pricesでループを回す
-   * 2. ループを回した
-   * 2.
-   *
-   */
-  execute(n: number, prices: number[]) {
-    for (const priceIndex in prices) {
-      if (Number(priceIndex) === 0) {
-        
-      }
-    }
+export function maximumProfit(lines: string[]) {
+  /// 整数n
+  let maxValue = -2000000;
+  let minValue = Number(lines[1]);
+  const values = lines.slice(1).map(l => Number(l));
+  for (const value of values) {
+    maxValue = Math.max(maxValue, value - minValue);
+    minValue = Math.min(minValue, value)
   }
+
+  console.log(maxValue)
 }
